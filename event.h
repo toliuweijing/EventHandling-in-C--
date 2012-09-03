d7cb4b1c2c3a76b496a1f81d1b411a766b524725
weijing <weijing.liu@nyu.edu>
2012-09-03 15:42:32 -0400

    version 1.0

---
+++ event.h
@@ -0,0 +1,83 @@
+/**********************************************************************
+   class:  Event (template class)
+   author: Weijing Liu
+   date:   May 21, 2013
+   Provides a fully functional "event" class, whose usage is exactly 
+   similar to the one in C#
+**********************************************************************/
+
+
+
+#ifndef WL_EVENT_H 
+#define WL_EVENT_H 
+
+
+
+
+
+
+#include<vector>
+#include"base_event.h"
+
+
+
+namespace wl {   // weijing liu(wl) library namespace
+    
+
+    /* This is a template class. It is to provide a mechanism
+       that broadcasts the happening event to its subscribers. The parameter 
+       1) SenderT is the event owner, 2) ArgT is the event info to tell
+       the event subscribers.
+     */
+    template <class SenderT, class ArgT>    class Event;
+    template <class SenderT, class ArgT>    
+    class Event<SenderT*,ArgT>
+    {  
+        
+        // event handlers of subcribers, which are the particular functions to call when the event occurs.
+        std::vector<details::Base_EventHandler<SenderT*,ArgT>* > _handlers;
+        
+
+        
+        
+        
+    public:
+        
+        Event(){}
+        
+
+        virtual ~Event()
+        {
+            for (unsigned i = 0; i < _handlers.size(); ++i) 
+            {
+                delete _handlers[i];
+                _handlers[i] = NULL;
+            }
+        }
+        
+        
+	// Event broadcast operator
+	// It is  called usually when the event occurs.
+        void operator()(SenderT* sender, ArgT args)
+        {
+            for (unsigned i = 0; i < _handlers.size(); ++i) 
+            {
+                (*_handlers[i])(sender, args);
+            }
+        }
+        
+        
+        
+	// Subscription interface for subscribers.
+        template <class ObjT>
+        void Add(ObjT *obj, void (ObjT::*memfun)(SenderT *, ArgT))
+        {
+            _handlers.push_back(new details::EventHandler<ObjT*, SenderT*, ArgT>(obj, memfun));    
+        }
+    };
+    
+
+}
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "event.h"
+#include <iostream>
+using namespace wl;
+using namespace std;
+
+struct args{
+};
+
+class A{
+public:
+  Event<A*, args> _event;
+};
+
+
+
+class B{
+public:
+  void HandleEvent(A* sender, args arg){
+    cout << "hello world" << endl;
+  }
+};
+
+int main()
+{
+  args arg;
+  A a;
+  B b;
+  a._event.Add(&b, &B::HandleEvent);
+  a._event(&a, arg);
+  return 0;
+}
